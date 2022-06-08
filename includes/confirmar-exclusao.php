5b786bfce634ba1307579eb95446d4ec696a5b5b
Fábio Marotti <105256021+DevMarotti@users.noreply.github.com>
2022-06-08 10:20:36 -0300

    Update confirmar-exclusao.php
    Alterando a apresentação do alert Excluir

--- includes/confirmar-exclusao.php
+++ includes/confirmar-exclusao.php
@@ -1,25 +1,21 @@
 <main>
 
-  <h2 class="mt-3">Excluir Contato</h2>
-
+  <div class="alert alert-danger  fade show" role="alert">
+    <h2 class="mt-2">Excluir Contato</h2>
+  </div>
+  
   <form method="post">
-
     <div class="form-group mt-5 mb-4">
       <div class="alert alert-warning alert-dimissible fade show" role="alert">
         Deseja realmente excluir o contato <strong><?=$objetoPessoa->pes_nome?></strong> ?
       </div>  
     </div>
-
     <div class="form-group">
       <a href="index.php">
         <button type="button" class="btn btn-secondary">Cancelar</button>
       </a>
-
       <button type="submit" name="excluir" class="btn btn-danger">Excluir</button>
     </div>
-
   </form>
-
-</div>
 
 </main>
